bb7fbe341de8857c0d9b0878a6807b16bec3e391
Aayushman <aayushman22september@gmail.com>
2026-03-05 08:55:10 +0530

    floor

---
+++ #floor function.py
@@ -0,0 +1,10 @@
+#floor function 
+#In Python, the floor function returns the largest integer less than or equal to a given number.
+
+##In simple words:
+#👉 It rounds the number down to the nearest whole number.
+
+a = 10
+b = 20
+floor_division = a // b
+print("floor division:" , a , "//" , b , "=", floor_division)
