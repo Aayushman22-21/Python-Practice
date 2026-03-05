b43103451599cb9461596251e760e9e76aed99a0
Aayushman <aayushman22september@gmail.com>
2026-03-05 08:54:51 +0530

    ch

--- Arithmetic_op.py
+++ Arithmetic_op.py
@@ -2,4 +2,17 @@ a = 10
 b = 20
 # addition
 addition = a + b
-print("addition", addition)
+print("addition:", a, "+", b ,"=", addition)
+# subtraction
+subtractionn = a - b 
+print("subtraction:", a , "-", b, "=", subtractionn)
+# multiplication 
+multi = a * b 
+print("multiplication:" , a , "*" , b , "=", multi)
+# division 
+divi = a / b 
+print("division:" , a , "/", b , "=", divi)
+
+#floor division 
+floor_division = a // b
+print("floor division:" , a , "//" , b , "=", floor_division)
